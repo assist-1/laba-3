32c2003d77862a5cce489bac480b3707e0181cf3
jootiee <purrrtov@ya.ru>
2022-11-27 18:03:16 +0000

    Added sort

--- main.cpp
+++ main.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include "sort.cpp"
+#include "parse.cpp"
 
-bool *get_args(int argc, char* argv[]) {
+bool* get_args(int argc, char* argv[]) {
     static bool flags[2] = {false, true};
 
     return flags;
 }
 
 
-float get_data(float * numbers[], int n, bool from_file) {
-    if (from_file) {
-        return 1;
-    }
-    return 0;
-}
-
-
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
     bool* flags = get_args(argc, argv);
-    std::cout << flags[0] << " " << flags[1] << std::endl;
+    // std::cout << flags[0] << " " << flags[1] << std::endl;
+
+    int T;
+    std::cin >> T;
+    float* lower = new float[T];
+    float* upper = new float[T];
+    float a, b;
+
+    for (int i = 0; i < T; ++i) {
+        std::cin >> a >> b;
+        lower[i] = a;
+        upper[i] = b;
+    }
+
+    sort(lower, T, upper, T);
+
+    for (int i = 0; i < T; ++i) {
+        std::cout << lower[i] << '\t' << upper[i] << std::endl;
+    }
+    
+    int ans_maxlen = T / 2 + 1;
+    float* ans_lower = new float[ans_maxlen];
+    float* ans_upper = new float[ans_maxlen];
+    
+    parse(lower, T, upper, T, ans_lower, ans_maxlen, ans_upper, ans_maxlen);
+
+    return 0;
 }
---
+++ parse.cpp
@@ -0,0 +1,7 @@
+
+
+
+float parse(float lower[], int n1, float upper[], int n2, 
+            float ans_low[], int n3, float ans_up[], int n4) {
+    return 0;
+}
