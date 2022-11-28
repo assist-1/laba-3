a11aad5faef62c6024ea66256b192fca203a98a8
jootiee <purrrtov@ya.ru>
2022-11-28 14:56:55 +0000

    Fixed file-read/out behaviour

--- main.cpp
+++ main.cpp
@@ -4,39 +4,41 @@
 #include "sort.cpp"
 #include "parse.cpp"
 
-bool* get_args(int argc, char* argv[], std::ifstream* input, std::ofstream* output) {
-    static bool flags[2] = {false,  // --fromfile
-                            false}; // --tofile
-    if (argc - 1) {
-        for (int i = 2; i < argc; ++i) {
-            if (!strcmp("--fromfile", argv[i])) {
-                std::ifstream input(argv[i + 1]);
-                if (!input) {
-                    std::cerr << "Wrong input file name. Try again." << std::endl;
-                    exit(-1);
-                }
+int main(int argc, char* argv[]) {
+    char* input_path;
+    char* output_path;
+    bool flags[2] = {false,  // --fromfile
+                     false}; // --tofile
+    int input_path_index;
+    int output_path_index;
+    if (argc > 1) {
+        for (int i = 1; i < argc; ++i) {
+            if (strcmp("--fromfile", argv[i]) == 0) {
+                input_path = argv[i + 1];
                 flags[0] = true;
                 ++i;
             }
-            if (!strcmp("--tofile", argv[i])) {
-                std::ofstream output(argv[i + 1]);
+            if (strcmp("--tofile", argv[i]) == 0) {
+                output_path = argv[i + 1];
                 flags[1] = true;
                 ++i;
             }
         }
     }
-    return flags;
-}
+    
+    std::ifstream input(input_path);
+    std::ofstream output(output_path);
 
-int main(int argc, char* argv[]) {
-    std::ifstream input;
-    std::ofstream output;
-    bool* flags = get_args(argc, argv, &input, &output);
+    if (!input) {
+        std::cerr << "Wrong input file name. Try again." << std::endl;
+        exit(-1);
+    }
+
     
     int T;
-
     if (flags[0]) input >> T;
     else std::cin >> T;
+
 
     float* lower = new float[T];
     float* upper = new float[T];
@@ -56,12 +58,20 @@ int main(int argc, char* argv[]) {
     
     if (ans_c) {
         for (int i = 0; i < ans_c; i += 2) {
-            if (flags[1]) output << ans[i] << ' ' << ans[i + 1] << std::endl;
-            else std::cout << ans[i] << ' ' << ans[i + 1] << std::endl;
+            if (flags[1]) {
+                output << ans[i] << ' ' << ans[i + 1] << std::endl;
+            }
+            else {
+                std::cout << ans[i] << ' ' << ans[i + 1] << std::endl;
+            }
         }
     }
     else {
-        if (flags[1]) output << "NOTHING FOUND" << std::endl;
+        if (flags[1]) {
+            output << "NOTHING FOUND" << std::endl;
+        }
         else std::cout << "NOTHING FOUND" << std::endl;
     }
+    if (flags[0]) input.close();
+    if (flags[1]) output.close();
 }
